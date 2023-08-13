4614cea0473f7c45e540cc10761947f737fab1eb
Shad Abdulsamad <shad.A.A2002@gmail.com>
2023-08-13 23:13:11 +0300

    new updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-function Footer(){
-    const currentYear = new Date().getFullYear();
-    return <footer>
 
-    <p>&copy; {currentYear}</p>
-
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright &copy; {year}</p>
     </footer>
+  );
 }
 
 export default Footer;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-function Note(props){
-    return <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+
+function Note(props) {
+
+  function handleDeleteButtonClick(){
+
+    props.onDelete(props.id);
+
+  }
+
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick = {handleDeleteButtonClick}>DELETE</button>
     </div>
+  );
 }
 
 export default Note;
