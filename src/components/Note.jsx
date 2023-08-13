51941693e8a9644318c4d98e26b4f65c2a8fa000
shad-abdulsamad <shad.A.A2002@gmail.com>
2023-08-13 23:54:10 +0300

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,19 +1,18 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function Note(props) {
-
-  function handleDeleteButtonClick(){
-
+  function handleClick() {
     props.onDelete(props.id);
-
   }
-
 
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick = {handleDeleteButtonClick}>DELETE</button>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
